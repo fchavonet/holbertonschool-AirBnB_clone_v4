1aad92b55efc4eb6ab29155da6f9a079a189b31d
fchavonet <fchavonet@mailo.com>
2024-02-05 20:43:39 +0100

    Implement dynamic amenity selection handling and update the `h4` tag when selected.

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+    let amenitiesSelected = {};
+
+    $('input[type="checkbox"]').change(function () {
+        const amenityId = $(this).closest('li').data('id');
+        const amenityName = $(this).closest('li').data('name');
+
+        if ($(this).is(':checked')) {
+            amenitiesSelected[amenityId] = amenityName;
+        } else {
+            delete amenitiesSelected[amenityId];
+        }
+
+        const amenitiesList = Object.values(amenitiesSelected).join(', ');
+        $('div.amenities h4').text(amenitiesList);
+    });
+});
